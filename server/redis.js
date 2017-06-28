dc00d686d669a44ca78bf19ce0671db87df817d3
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-06-28 14:05:35 +0100

    update redis conf
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/redis.js
+++ server/redis.js
@@ -3,9 +3,12 @@
 var redis = require('redis');
 
 function _newRedisClient(port, server) {
+  port = port || process.env.REDIS_PORT;
+  server = server || process.env.REDIS_URL;
+
   var client = redis.createClient(port, server);
 
-  client.once('ready', function () {
+  client.once('ready', function() {
     console.log('Redis is connected');
   });
 
@@ -13,11 +16,11 @@ function _newRedisClient(port, server) {
   //   console.log('Redis error:', err);
   // });
 
-  client.on('reconnecting', function () {
+  client.on('reconnecting', function() {
     console.log('Redis is reconnecting');
   });
 
-  client.once('end', function () {
+  client.once('end', function() {
     console.log('Redis connection terminated');
   });
 
