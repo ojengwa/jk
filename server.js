a6a895a7ade16204cb1613390acc3434517ee659
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-06-28 14:28:30 +0100

    update server bootstrap
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ var playerManager = require('./server/player_manager');
 
 require('./server/api')(app, playerManager);
 
-http.listen();
+http.listen(process.env.PORT || port, function() {
+	console.log('✔ App listening on port', process.env.PORT || config.port);
+});
