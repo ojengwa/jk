0b9746aadbcbfd0712894dc77d80c6178fe35a11
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-06-28 12:15:58 +0100

    update tracklist actions: add track add function
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/js/actions/TracklistActions.js
+++ public/js/actions/TracklistActions.js
@@ -5,30 +5,36 @@ var TracklistConstants = require('../constants/TracklistConstants');
 var api = require('../utils/api');
 
 var TracklistActions = {
-  fetch: function () {
-    api.tracks.get(function (err, tracks) {
+  fetch: function() {
+    api.tracks.get(function(err, tracks) {
       if (err) return console.error(err);
       TracklistActions.set(tracks);
     }.bind(this));
   },
-  set: function (tracks) {
+  set: function(tracks) {
     AppDispatcher.handleViewAction({
       actionType: TracklistConstants.TRACKLIST_SET,
       data: tracks
     });
   },
-  add: function (track) {
+  add: function(track) {
     AppDispatcher.handleViewAction({
       actionType: TracklistConstants.TRACKLIST_ADD,
       data: track
     });
   },
-  addUrl: function (trackUrl) {
-    api.tracks.post({ url: trackUrl }, function (err) {
-      if (err) return console.error(err);
+  addUrl: function(trackUrl) {
+    console.log(trackUrl)
+    api.tracks.post({
+      url: trackUrl
+    }, function(err) {
+      if (err) {
+        console.log(err);
+        return console.error(err);
+      }
     });
   },
-  remove: function (index) {
+  remove: function(index) {
     AppDispatcher.handleViewAction({
       actionType: TracklistConstants.TRACKLIST_REMOVE,
       data: index
