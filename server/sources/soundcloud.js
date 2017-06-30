c202edfefde79951b790a9c5b9dc299a92290fce
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-06-30 16:49:03 +0100

    update soundcloud source
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/sources/soundcloud.js
+++ server/sources/soundcloud.js
@@ -5,6 +5,7 @@ var urlParser = require('url');
 var https = require('https');
 var Throttle = require('throttle');
 var Track = require('../track');
+var logger = require('../logger');
 var Q = require('q');
 var unirest = require('unirest');
 var config = require('../config');
@@ -50,31 +51,32 @@ function resolve(trackUrl) {
   var deferred = Q.defer();
 
   unirest.get('https://api.soundcloud.com/resolve.json')
-  .query({
-    client_id: config.soundcloud.clientId,
-    url: trackUrl
-  })
-  .end(function (response) {
-    if (response.error) return deferred.reject(response.error);
-    if (response.body.kind === 'track') {
-      var track = response.body;
-      // Better image resolution
-      track.artwork_url = track.artwork_url.replace('large.jpg', 't300x300.jpg');
-      track.bitrate = 128 * 1000;
-      deferred.resolve(new SoundcloudTrack(track));
-    } else if (response.body.kind === 'playlist') {
-      var tracks = response.body.tracks.map(function (tr) {
+    .query({
+      client_id: config.soundcloud.clientId,
+      url: trackUrl
+    })
+    .end(function(response) {
+      logger.log(response.body.items);
+      if (response.error) return deferred.reject(response.error);
+      if (response.body.kind === 'track') {
+        var track = response.body;
         // Better image resolution
-        tr.artwork_url = tr.artwork_url.replace('large.jpg', 't300x300.jpg');
-        tr.bitrate = 128 * 1000;
-        return new SoundcloudTrack(tr);
-      });
+        track.artwork_url = track.artwork_url.replace('large.jpg', 't300x300.jpg');
+        track.bitrate = 128 * 1000;
+        deferred.resolve(new SoundcloudTrack(track));
+      } else if (response.body.kind === 'playlist') {
+        var tracks = response.body.tracks.map(function(tr) {
+          // Better image resolution
+          tr.artwork_url = tr.artwork_url.replace('large.jpg', 't300x300.jpg');
+          tr.bitrate = 128 * 1000;
+          return new SoundcloudTrack(tr);
+        });
 
-      deferred.resolve(tracks);
-    } else {
-      deferred.reject('This is not a track.');
-    }
-  });
+        deferred.resolve(tracks);
+      } else {
+        deferred.reject('This is not a track.');
+      }
+    });
 
   return deferred.promise;
 }
@@ -82,18 +84,18 @@ function resolve(trackUrl) {
 /**
  * Private helpers
  */
- SoundcloudTrack.prototype._initFromExternal = function (track) {
-  this.title     = track.title;
-  this.artist    = track.user.username;
-  this.duration  = track.duration;
-  this.url       = track.permalink_url;
+SoundcloudTrack.prototype._initFromExternal = function(track) {
+  this.title = track.title;
+  this.artist = track.user.username;
+  this.duration = track.duration;
+  this.url = track.permalink_url;
   this.streamUrl = track.stream_url;
-  this.cover     = track.artwork_url;
+  this.cover = track.artwork_url;
   this.createdAt = new Date();
-  this.platform  = 'soundcloud';
+  this.platform = 'soundcloud';
 };
 
-SoundcloudTrack.prototype._initFromInternal = function () {
+SoundcloudTrack.prototype._initFromInternal = function() {
   SoundcloudTrack.super_.apply(this, arguments);
 };
 
@@ -102,7 +104,7 @@ SoundcloudTrack.prototype._initFromInternal = function () {
  * Also performs recurrently to follow redirections.
  * Emits `progress` events.
  */
- SoundcloudTrack.prototype._download = function (streamUrl, position) {
+SoundcloudTrack.prototype._download = function(streamUrl, position) {
   // do not add the clientId after a redirection
   if (streamUrl.indexOf('Key-Pair-Id') < 0) {
     streamUrl += '?client_id=' + config.soundcloud.clientId;
@@ -120,12 +122,12 @@ SoundcloudTrack.prototype._initFromInternal = function () {
     options.headers.Range = 'bytes=0-';
   }
 
-  var output = new Throttle(1.05 * 128*1000/8); // throttle at 128kbps
+  var output = new Throttle(1.05 * 128 * 1000 / 8); // throttle at 128kbps
 
-  https.get(options, function (res) {
+  https.get(options, function(res) {
     if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
       return this._download(res.headers.location, position).pipe(output);
-    } else if (res.statusCode >= 400 ) {
+    } else if (res.statusCode >= 400) {
       // 404 or whatever, we skip
       output.end();
       return;
@@ -142,11 +144,17 @@ SoundcloudTrack.prototype._initFromInternal = function () {
       currentLength = 0;
     }
 
-    this.emit('progress', { current: currentLength, total: totalLength });
+    this.emit('progress', {
+      current: currentLength,
+      total: totalLength
+    });
 
-    res.on('data', function (chunk) {
+    res.on('data', function(chunk) {
       currentLength += chunk.length;
-      this.emit('progress', { current: currentLength, total: totalLength });
+      this.emit('progress', {
+        current: currentLength,
+        total: totalLength
+      });
     }.bind(this)).pipe(output);
 
   }.bind(this)).end();
