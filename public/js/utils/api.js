f4e98289e3c51fa2466108416d120e1c50062cfe
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-06-30 16:47:40 +0100

    update api utils
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- public/js/utils/api.js
+++ public/js/utils/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var api = {
-  req: function (method, url, data, callback) {
+  req: function(method, url, data, callback) {
     if (!callback) {
       callback = data;
       data = null;
@@ -11,51 +11,52 @@ var api = {
       url: url,
       data: data,
       type: method,
-      success: function (data) {
+      success: function(data) {
         callback(null, data);
       },
-      error: function (xhr, status, err) {
+      error: function(xhr, status, err) {
         callback(err);
       }
     });
   },
   player: {
-    get: function (callback) {
+    get: function(callback) {
       api.req('GET', '/player', callback);
     },
-    post: function (callback) {
+    post: function(callback) {
       api.req('POST', '/player', callback);
     },
-    delete: function (callback) {
+    delete: function(callback) {
       api.req('DELETE', '/player', callback);
     }
   },
   tracks: {
-    get: function (callback) {
+    get: function(callback) {
       api.req('GET', '/tracks', callback);
     },
-    post: function (track, callback) {
+    post: function(track, callback) {
+      console.log(track, callback)
       api.req('POST', '/tracks', track, callback);
     }
   },
   current: {
-    get: function (callback) {
+    get: function(callback) {
       api.req('GET', '/current', callback);
     },
-    delete: function (callback) {
+    delete: function(callback) {
       api.req('DELETE', '/current', callback);
     }
   },
   votes: {
-    get: function (callback) {
+    get: function(callback) {
       api.req('GET', '/votes', callback);
     },
-    post: function (callback) {
+    post: function(callback) {
       api.req('POST', '/votes', callback);
     },
   },
   history: {
-    get: function (callback) {
+    get: function(callback) {
       api.req('GET', '/history', callback);
     }
   }
